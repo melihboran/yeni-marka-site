b94a49694b44121b08dda8ab65e63206bfe7d75f
Melihboran <melihboran@192.168.1.61>
2026-06-23 18:27:57 +0300

    ilk sürüm

---
+++ en/assets/vendor/app.forms.js
@@ -0,0 +1,67 @@
+﻿/* formu gönder */
+$(".btn-send-data").on("click", function () {
+
+    let prefix = "." + $(this).data("prefix") + "-form-data";
+    let formID = $(this).data("formid");
+    let params = [{ id: formID, properties: [] }];
+    let canSend = true;
+    //------------------------------------------
+    let person = "";
+    $(prefix).each(function () {
+        let dataID = $(this).attr("id");
+        let dataName = $(this).data("field");
+        let dataType = $(this).attr("type");
+        let dataValue = $(this).val();
+        //------------------------------------------
+        if (dataID != "Kvkk" && dataValue != null && dataValue != "") {
+            if (dataID == "NameSurname")
+                person = dataValue;
+            //------------------------------------------
+            if (dataType == "file") {
+                let dataFiles = new FormData();
+                let files = $(this).get(0).files;
+                if (files.length > 0) {
+                    dataFiles.append("Title", person);
+                    dataFiles.append("UploadedFile", files[0]);
+                    dataValue = postAjaxFile(dataFiles);
+                }
+                else
+                    return true;
+            }
+            //------------------------------------------
+            if (dataType == "checkbox") {
+                if ($(this)[0].checked == true)
+                    params[0].properties.push({ "id": dataID, "type": dataType, "name": dataName, "value": "Evet" });
+                else
+                    params[0].properties.push({ "id": dataID, "type": dataType, "name": dataName, "value": "Hayır" });
+            }
+            else
+                params[0].properties.push({ "id": dataID, "type": dataType, "name": dataName, "value": dataValue });
+
+        }
+        //------------------------------------------
+        else if ($(this).prop('required') && $(this).prop('checked') != true) {
+            canSend = false;
+            alert(getLang(2), getLang(4), "warning");
+        }
+    });
+    //------------------------------------------
+    if (canSend) {
+        swal({
+            title: "Form gönderiliyor.",
+            text: "Bilgileriniz işleniyor lütfen bekleyiniz...",
+            timer: 2000,
+            button: {
+                visible: false,
+            },
+        }).then(function () {
+            let data = postAjaxData("Connect", params);
+            if (data.d == "true")
+                alertWithRedirect(getLang(1), getLang(6), "success", window.location.href);
+            else if (data.d == "false")
+                alert(getLang(3), getLang(7), "error");
+            else
+                alert(getLang(3), data.d, "error");
+        }), 4000;
+    }
+});
---
+++ en/assets/vendor/app.js
@@ -0,0 +1,116 @@
+﻿/* genel uyarı şablonu */
+function alert(Ititle, Itext, Iicon) {
+    swal({
+        title: Ititle,
+        text: Itext,
+        icon: Iicon,
+        button: {
+            visible: false,
+        },
+    });
+};
+
+/* uyarı sonrası yönlendirme */
+function alertWithRedirect(Ititle, Itext, Iicon, IUrl) {
+    swal({
+        title: Ititle,
+        text: Itext,
+        icon: Iicon,
+        button: {
+            visible: false,
+        },
+        timer: 3000
+    }).then(function () {
+        window.location = "" + IUrl + "";
+    }), 400
+};
+
+/* ajax: id ve dil kodu verilen bilginin karşılığını language.json'dan getirir. */
+function getLang(byID) {
+    return JSON.parse($.ajax({ url: '/assets/language.json', async: false }).responseText).filter(x => x.ID == byID)[0]["TR"];
+}
+
+/* post ajax */
+function postAjax(PageURL, Parameters) {
+    return JSON.parse($.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "/AjaxRequest.asmx/" + PageURL,
+        data: Parameters,
+        datatype: "json",
+        async: false
+    }).responseText);
+}
+
+/* post ajax data */
+function postAjaxData(pageurl, dataString) {
+    let paramaters = JSON.stringify({ value: dataString });
+    return JSON.parse($.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "/AjaxRequest.asmx/" + pageurl,
+        data: paramaters,
+        datatype: "json",
+        async: false
+    }).responseText);
+}
+
+/* post ajax file */
+function postAjaxFile(dataString) {
+    let returningValue = "false";
+    $.ajax({
+        type: "POST",
+        url: "/AjaxRequest.asmx/SaveFile",
+        data: dataString,
+        contentType: false,
+        processData: false,
+        async: false,
+        success: function (data) {
+            returningValue = data.documentElement.textContent;
+        },
+        error: function () {
+            returningValue = "false";
+        }
+    });
+    return returningValue;
+}
+
+/* dropdown doldur */
+function FillDropdown(id, drpname, firstitem, data) {
+    let drpDropdown = $("[id*=" + drpname + "]");
+    drpDropdown.empty().append('<option selected="selected" value="0">' + firstitem + '</option>');
+    $.each(data, function () {
+        if (id != "") {
+            if (this['Value'] == id) { drpDropdown.append($("<option selected></option>").val(this['Value']).html(this['Text'])); }
+            else { drpDropdown.append($("<option></option>").val(this['Value']).html(this['Text'])); }
+        }
+        else { drpDropdown.append($("<option></option>").val(this['Value']).html(this['Text'])); }
+    });
+}
+
+/* search */
+var input = document.getElementById("SiteSearch");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        Search();
+    }
+});
+input.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        Search();
+    }
+});
+
+function Search() {
+    var veriarama = $("#SiteSearch").val()
+    veriarama = $.trim(veriarama)
+    if (veriarama == "") {
+        alert(getLang(2), getLang(4), "warning");
+        return false;
+    }
+    else {
+        window.location = "/arama/" + veriarama;
+        return false;
+    }
+}
